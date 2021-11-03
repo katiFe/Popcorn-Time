f96f88b3c7ce4e1f2b316fcd429080dd0d8b71bd
Kati Fe <k-felgitsch@hotmail.com>
2021-11-03 19:02:07 +0000

    creating button to display only movie classics using state

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,21 +4,46 @@ import Movie from "./Movie"
 
 
 
-function Main() {
 
-    return (  
+class Main extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            moviesToDisplay: moviesArr
+        }
+    };
+
+renderClassicsOnly = ()=>{
+    this.setState((prevState, props)=>{
+        const newList = prevState.moviesToDisplay.filter((movie)=>{
+            return movie.year < 2000 
+        });
+
+        return {moviesToDisplay: newList}
+    });
+}
+
+render(){
+   return (
         <>
-    {moviesArr.map((movie)=>{
-      return  <Movie  
-        key={movie.id}                                 //JSX spread attribute also valid {...movie} sending all the properties of the object
-          title= {movie.title}
-          year= {movie.year}
-          rating={movie.rating}
-      /> ;
-    })}
-            
+            <div className="controls">
+                <button onClick={this.renderClassicsOnly}>Display only classics</button>
+            </div>
+            <div className="movie-list">
+                {this.state.moviesToDisplay.map((movie) => {
+                    return <Movie
+                        key={movie.id}                                 //JSX spread attribute also valid {...movie} sending all the properties of the object
+                        title={movie.title}
+                        year={movie.year}
+                        rating={movie.rating}
+                    />;
+                })}
+
+            </div>
         </>
-    );
+    ); 
+}
+    
 };
 
 export default Main;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ function Movie(props){
 
     const isPopular = props.rating;
 
-    function Popular(props){
+    function Popular(){
         if( isPopular > 8 ){
             return "POPULAR"
         }
@@ -19,8 +19,12 @@ function Movie(props){
             <p>Year: {props.year}</p>
             <p>Rating: {props.rating}</p>
             <p><strong>{Popular(props)}</strong></p>
-            {/*another alternative: Internal operator condition && result */}
+
+            {/*another alternative: logical operator {condition tue && then result} */}
             {/* {props.rating > 8 && <p className=""><stron>POPULAR</stron></p>} */}
+
+            {/*another alternative: operator*/}
+            {/* {props.rating > 8 ? <p className=""><stron>POPULAR</stron></p> : ""} */}
         </div>
     );
 };
