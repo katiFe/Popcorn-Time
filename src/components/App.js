c38ae773918ebeda0c98b847ce7eb20379691ff2
Kati Fe <k-felgitsch@hotmail.com>
2021-11-03 11:55:08 +0000

    creating components

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Main from './Main';
+import Footer from './Footer';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Main/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return <p>This is the Footer</p>
+
+    
+};
+
+
+export default Footer;
