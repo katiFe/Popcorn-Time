dabd62c870424c18124f50043b42d21420edc32f
Kati Fe <k-felgitsch@hotmail.com>
2021-11-03 13:52:54 +0000

    display multiple movies using .map()

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,40 @@
 import React from "react";
 
-function Main(){
-    return <p>This is the Main</p>
+
+
+function Main() {
+
+    const moviesArr = [
+        {
+            id: 1,
+            title: "Billy Elliot",
+            rating: 9
+        },
+        {
+            id: 2,
+            title: "Forest Gump",
+            rating: 9
+        },
+        {
+            id: 3,
+            title: "The Lion King",
+            rating: 10
+        }
+
+    ];
+
+    return (
+        <>
+            {moviesArr.map((movie, index) => {
+                return (
+                    <div key={movie.id}>
+                        <h2>Title: {movie.title}</h2>
+                        Rating: {movie.rating}
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default Main;
