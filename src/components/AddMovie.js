269e1d384b73566493ef6e84a4233b53c667628f
Kati Fe <k-felgitsch@hotmail.com>
2021-11-04 11:47:35 +0000

    Functionality to add a new movie: Controlled component with multiple … …input fields

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -5,16 +5,31 @@ import './AddMovie.css';
     constructor(props) {
         super(props);
         this.state = {
-            title: ""                      
+            title: "",
+            year: "",
+            rating: ""                      
         }
     }
 
-    handleTitleChange = ((event)=>{
-        // console.log("user is typing")
-        // console.log(event)
-        // console.log("user is typing ", event.target.value)
-        this.setState({title : event.target.value})
-    });
+    // handleTitleChange = ((event)=>{
+    //     // console.log("user is typing")
+    //     // console.log(event)
+    //     // console.log("user is typing ", event.target.value)
+    //     this.setState({title : event.target.value})
+    // });
+
+    // handleYearChange = ((event)=>{
+    //     this.setState({year: event.target.value})
+    // })
+
+    // handleRatingChange = ((event)=>{
+    //     this.setState({rating : event.target.value})
+    // })
+
+    handleInputChange = ((event)=>{
+        const nameOfTheInput = event.target.name;
+        this.setState({[nameOfTheInput] : event.target.value})
+    })
 
     render() {
         return (
@@ -27,7 +42,29 @@ import './AddMovie.css';
                     type="text"    
                     name="title" 
                     value={this.state.title} 
-                    onChange={this.handleTitleChange}/>
+                    onChange={this.handleInputChange}/>
+                    </label>
+
+                    <label>
+                    Year:
+                  <input 
+                    type="number"    
+                    name="year" 
+                    min="1930"
+                    max="2025"
+                    value={this.state.year} 
+                    onChange={this.handleInputChange}/>
+                    </label>
+
+                    <label>
+                    Rating:
+                  <input 
+                    type="number"    
+                    name="rating" 
+                    min="1"
+                    max="10"
+                    value={this.state.rating} 
+                    onChange={this.handleInputChange}/>
                     </label>
 
 
